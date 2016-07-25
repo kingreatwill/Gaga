1c4a8cdc61a863a34d7e57fa278e714153b8f32b
kingreatwill <350840291@qq.com>
2016-07-25 17:18:52 +0800

    添加项目

---
+++ Gaga.Core/Logging/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gaga.Core.Logging
+{
+	public enum LogLevel
+	{
+		Debug,
+		Information,
+		Warning,
+		Error,
+		Fatal
+	}
+
+	public interface ILogger
+	{
+		bool IsEnabled(LogLevel level);
+		void Log(LogLevel level, Exception exception, string format, params object[] args);
+	}
+}
